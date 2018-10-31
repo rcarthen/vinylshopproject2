288edce072d9dd04b64f68422ac4486d2bf90bf9
Ruth <rcarthen@gmail.com>
2018-10-31 13:01:03 -0600

    seeder data

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,27 @@
+
+// $("#login").on("click", function (event) {
+
+//   event.preventDefault()
+//   console.log("click works");
+
+//   const userInfo = {
+//       username: $("#username").val(),
+//       email: $("email").val(),
+//      password: $("#password").val()
+
+//   }
+
+
+
 $(document).ready(function(){
-    $('.carousel').carousel();
-  });
-      
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
+
+$('#button name').on('click', function(req, res){
+  console.log('');
+});
+
---
+++ seeder/seeder.js
@@ -0,0 +1,94 @@
+const db = require('../models');
+
+const items = [
+
+        {
+            product_name: 'The Beatles Yellow Submarine',
+            department_name: "Vinyls",
+            price: 6000,
+            quantity: 3,
+            image: 'https://target.scene7.com/is/image/Target/GUEST_85c13716-9d6c-4a16-a87b-75623e46afad?wid=488&hei=488&fmt=webp'
+        },
+
+        {
+            product_name: 'Record Player',
+            department_name: 'Vinyl Technology',
+            price: 150,
+            quantity: 5,
+            image:'https://images.urbanoutfitters.com/is/image/UrbanOutfitters/46726774_012_b?$xlarge$&hei=900&qlt=80&fit=constrain'
+        },
+
+        {
+            product_name: 'Solid Teak Record Cabinet',
+            department_name: 'Vinyl Organization',
+            price: 300,
+            quantity: 11
+        },
+
+        {
+            product_name: 'Three Little Birds T-Shirt',
+            department_name: 'Apparel',
+            price: 25,
+            quantity: 100,
+            image: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjStsqIobHeAhXSmuAKHQmjAL4QjRx6BAgBEAU&url=http%3A%2F%2Fwww.rastaempire.com%2Fp-4727-bob-marley-groovy-one-love-black-t-shirt-youth.aspx&psig=AOvVaw1uMUTqGY0kOOVTTfkqB7H2&ust=1541095165960763'
+        },
+
+        {
+            product_name: 'Purple Rain',
+            department_name: "Vinyls",
+            price: 250,
+            quantity: 3
+        },
+        {
+            product_name: 'Mama Miriam Makeba T-Shirt',
+            department_name: "Apparel",
+            price: 25,
+            quantity: 100
+        },
+
+        {
+                product_name: 'Rose Gold Vinyl Frame',
+                department_name: 'Vinyl Organization',
+                price: 25,
+                quantity: 100
+            }, {
+                product_name: 'Elvis Presley Jailhouse Rock',
+                department_name: 'Vinyls',
+                price: 25,
+                quantity: 100
+            }, {
+                product_name: 'LP Vinyl Record UltraSonic Cleaner',
+                department_name: 'Vinyl Technlogy',
+                price: 25,
+                quantity: 100
+            },
+
+            {
+                product_name: 'KISS Hoodie',
+                department_name: 'Apparel',
+                price: 25,
+                quantity: 100
+            },
+
+            {
+                product_name: 'Led Zepplin',
+                department_name: 'Vinyls',
+                price: 25,
+                quantity: 100
+            }
+
+
+
+
+        ];
+
+
+        db.sequelize.sync({
+            force: true
+        }).then(function () {
+            db.products.bulkCreate(items).then(function (rows) {
+                console.log('\n\nINSERTED\n\n');
+            }).catch(function (err) {
+                console.log('\n\nError:', err);
+            });
+        });
