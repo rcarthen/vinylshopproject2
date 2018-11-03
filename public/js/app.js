8f66a85c216a0e8f099d429ac9dc621f67573f58
Ruth <rcarthen@gmail.com>
2018-11-02 21:01:26 -0600

    vinyl shop

--- public/js/app.js
+++ public/js/app.js
@@ -25,29 +25,58 @@ $('#button name').on('click', function(req, res){
   console.log('');
 });
 
-$(document).ready(function() {
-  $('select').material_select();
-});
-
-
 $.ajax({
     
   url: "/api/vinyl",
   method: "GET",
 }) .then(function(response){
-console.log(response)
   for(let i = 0; i < response.length; i++){
       let data = response[i]
-      $("#vinylview").append(data.vinyl_name);
-      $("#vinylview").append(data.artist);
-      $("vinylview").append(data.price)
+      // $("#displayrow").append(data.vinyl_name);
+      // $("#displayrow").append(data.artist);
+      // $("#displayrow").append(data.price);
+      // $("#displayrow").append(data.genre);
+      // $("#displayrow").append(data.quantity);
+      // $("#displayrow").append(data.image);
+
+      let vinyl_name= data.vinyl_name
+      let artist = data.artist
+      let genre= data.genre
+      let price= data.price
+      let quantity= data.quantity
+      let image= data.image 
+    
+
+      $ ("#displayrow").append( 
+      `<div class="card col s4">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" id="image" src="${image}">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">${vinyl_name}<i class="material-icons right">more_vert</i></span>
+            <p><a href="#"><i class="medium material-icons"></i></a></p>
+            <a class="waves-effect waves-teal btn-flat">ADD TO BAG</a>
+          </div>
 
 
-  }
+          
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">${vinyl_name}<i class="material-icons right">close</i></span>
+            <p id="description">"Music Description"</p>
+            <div class="card-action">
+              <a class="waves-effect waves-teal btn-flat">ADD TO BAG</a>
+              <div class="card col s10">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" id="image" src="${image}">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`
 
- console.log(response);
+     )
 
-})
-
-
-                    
+      }
+    
+    })
+   
